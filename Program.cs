0a856072a7a5cdd8e2b5b4b57ca4be9ab27b4def
EderPaulo <paulofiuza24@gmail.com>
2024-10-11 09:58:59 -0300

    Usando o try-catch

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+try
+{
+    int[] numbers = { 1, 2, 3 };
+    Console.WriteLine(numbers[5]); // Isso vai gerar uma exceção
+
+    Salver("");
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Erro: Índice fora do intervalo do array.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Ocorreu um erro: " + ex.Message);
+}
+
+static void Salver(String texto)
+{
+
+    if (string.IsNullOrEmpty(texto))
+    {
+        throw new Exception("O texto não pode ser nulo ou vazio");
+    }
+}
